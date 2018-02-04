d2a7b34e38bac949417520f82b7ef6daa38afbee
Olivier <olivier.kremer@free.fr>
2018-02-04 18:21:56 +0100

    Bootstrap - initial load

--- entrypoint.bash
+++ entrypoint.bash
@@ -11,6 +11,10 @@
 #       before executing.
 #
 
+declare ERR_UNSUPPORTED_OS=1
+declare ERR_MISSING_LIB=2
+declare ERR_INSUFFICIENT_PRIVILEGES=3
+
 # Inspired by https://github.com/progrium/bashstyle
 initBestPractices() {
     # Very basic debug mode
@@ -35,23 +39,105 @@ logSetup() {
     exec 2>&1
 }
 # A stupid log function, that print the given parameters in stdout (but add a timestamp)
+# $1 (String) -> the message to log
 log() {
-    printf "[$(date '+%Y-%m-%d-%H-%M-%S')]: $* \n"
+    printf "[$(date '+%Y-%m-%d-%H-%M-%S')]-$* \n"
 }
 # A stupid log function, that print the given parameters in stdout (but add [INFO]- and a timestamp )
+# $1 (String) -> the information message
 logInfo() {
-    log "[INFO]- $* \n"
+    log "[INFO ]: $*"
 }
 # A stupid log function, that print the given parameters in stdout (but add [WARN]- and a timestamp )
+# $1 (String) -> the warning message
 logWarn() {
-    log "[WARN]- $* \n"
+    log "[WARN ]: $*"
+}
+# A stupid log function, that print the given parameters in stdout (but add [ERROR]- and a timestamp )
+# $1 (String) -> the error message
+logError() {
+    log "[ERROR]: $*"
+}
+# A stupid log function, that print the given parameters in stdout (but add [FATAL]- and a timestamp )
+# Then exist with the provided error code.
+# $1 (int)    -> the exit error code
+# $2 (String) -> the error message
+logFatalError() {
+    local errorCode=$1
+    log "[FATAL]: A fatal error occurred. Error code: ${errorCode}"
+    shift
+    log "[FATAL]: $*"
+    exit ${errorCode}
 }
 
+# Check that a binary is present in the path. Exit with error code ERR_MISSING_LIB otherwise.
+# $1 -> the executable to find in the path.
+# Sample usage:
+#    checkBinaryIsInThePath wget
+checkBinaryIsInThePath() {
+    local binaryFile="$1"
+    if ! which "${binaryFile}" > /dev/null; then
+        logFatalError $ERR_MISSING_LIB "The tool '${binaryFile}' can't be found in the path. Are you sure your OS is officially supported ?"
+    fi
+}
 
+# Get the linux distribution identifier (ie: ubuntu, redhat, etc.)
+getDistribution() {
+	local dist=""
+	# Every system that we officially support has /etc/os-release
+	if [ -r /etc/os-release ]; then
+		dist="$(. /etc/os-release && echo "$ID")"
+    	dist="$(echo "$dist" | tr '[:upper:]' '[:lower:]')"
+	fi
+	echo "$dist"
+}
+
+# Currently, only basic checks are done. Will have to be improved in the future.
+checkOSIsSupported() {
+    local dist="$(getDistribution)"
+	case "${dist}" in
+		ubuntu)
+		    ;;
+		debian|raspbian)
+		    ;;
+	    *)
+            logFatalError $ERR_UNSUPPORTED_OS "Can't find the /etc/os-release file. Are you sure your OS is officially supported ?"
+            ;;
+    esac
+    checkBinaryIsInThePath sudo
+    checkBinaryIsInThePath apt-get
+    set +e
+    sudo ls > /dev/null
+    local errorCode=$?
+    set -e
+    if [ $errorCode -ne 0 ]; then
+        logFatalError $ERR_INSUFFICIENT_PRIVILEGES "Are your user a register as sudoered ? (See file /etc/sudoers)."
+    fi
+}
+
+updateOS() {
+    sudo apt-get update
+    sudo apt-get -y upgrade
+}
+
+installNeededPackages() {
+    sudo apt-get -y install wget make
+}
+
+installDocker() {
+    curl -fsSL get.docker.com | sh
+}
+
+# The main entry point of this script.
 main() {
     initBestPractices
     logSetup
-    log "Start Brother in Arm's project bootstrap\n"
+    log "Start Brother in Arm's project bootstrap"
+    checkOSIsSupported
+    updateOS
+    installNeededPackages
+    installDocker
+    log "Brother in Arm's project bootstrap completed"
 }
 
 # Don't run the main function if the this file is 'sourced', which is the case with bash_unit. So next line is a MUST for runnable script (not necessary for bash libraries)
@@ -62,26 +148,40 @@ main() {
 ### Test functions, designed to be run with bash_unit (https://github.com/pgrange/bash_unit)
 # Called before all tests (only once)
 setup_suite() {
-    printf "setup_suite\n"
+    printf "=>\n"
 }
 
 # Called before each test
 setup() {
-    printf "setup\n"
+    printf "==>\n"
 }
 
-test_main() {
-    main
+test_logFramework() {
+    initBestPractices
+    logSetup
+    log "Test log feature"
+    logInfo "Test log feature - info"
+    logWarn "Test log feature - warn"
+    logError "Test log feature - error"
     printf "LOGFILE: ${LOGFILE} \n"
     assert "test -f '${LOGFILE}'"
 }
 
+test_checkBinaryIsInThePath() {
+    assert_status_code 0 "checkBinaryIsInThePath curl"
+    assert_status_code $ERR_MISSING_LIB "checkBinaryIsInThePath curlfg"
+}
+
+test_checkOSIsSupported() {
+    assert_status_code 0 "checkOSIsSupported"
+}
+
 # Called after each test
 teardown() {
-    printf "teardown\n"
+    printf "<==\n"
 }
 
 # Called after all tests (only once)
 teardown_suite() {
-    printf "teardown_suite\n"
+    printf "<=\n"
 }
